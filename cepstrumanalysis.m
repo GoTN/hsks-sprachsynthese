b438b1863c6c0fdd083e0834ef36aeb4ca7f1f6d
funjetfan <petersteiner92@t-online.de>
2015-06-09 23:27:48 +0200

    erfolglose iteration über alle dateien....

--- cepstrumanalysis.m
+++ cepstrumanalysis.m
@@ -6,56 +6,44 @@
 %function y=cepstrumanalysis()
 
 % Einlesen einer Datei
-signal=wavread('a-karl.wav');
+signal=wavread('a-marcus.wav');
+x = dir("*.wav")
 
 % Anzahl der Punkte für die spaetere FFT
 nf=2^14;
 
-% Normierung, zeitliche Begrenzung der Aufnahme
-%signal = signal./max(abs(signal));
-signal = signal((end-nf)/2:(end+nf-1)/2);
+for audio_files = 1:length (x)   # loop over wave files
+  signal = wavread (x(audio_files).name);   # read audio file
 
-% Fensterung der Aufnahme mit einem Hanning-Fenster
-h_window = hanning(nf);
-signal = signal.*h_window;
+	% Normierung, zeitliche Begrenzung der Aufnahme
+	%signal = signal./max(abs(signal));
+	signal = signal((end-nf)/2:(end+nf-1)/2);
 
-% Berechnung des cepstrums
-spectrum = fft(signal);
-log_spectrum = log10(abs(spectrum));
-cepstrum = real(ifft(log_spectrum));
-%x_axis = (1:size(cepstrum));
+	% Fensterung der Aufnahme mit einem Hanning-Fenster
+	h_window = hanning(nf);
+	signal = signal.*h_window;
 
-% Fensterung des cepstrums, vorerst mit 0.5
-cepstrum = cepstrum(1:size(cepstrum)/2);
-%plot(cepstrum);
+	% Berechnung des cepstrums
+	spectrum = fft(signal);
+	log_spectrum = log10(abs(spectrum));
+	cepstrum = ifft(log_spectrum);
+	%x_axis = (1:size(cepstrum));
 
-% Liftering (Filterung des Vokaltraktsignals mit Formanten)
-lift = zeros(length(cepstrum),1);
-length(cepstrum)
-lift(1:length(cepstrum)/400) = 1;
-lift_cepstr = cepstrum.*lift;
-mag_spec = real(fft(lift_cepstr));
-plot(mag_spec);
+	% Fensterung des cepstrums, da das Cepstrum symmetrisch ist
+	cepstrum = cepstrum(1:size(cepstrum)/2);
+	%plot(cepstrum);
+	% Liftering (Filterung des Vokaltraktsignals mit Formanten)
+	lift = zeros(length(cepstrum),1);
+	lift(1:50) = 1;
+	lift_cepstr = real(cepstrum.*lift);	
+	ceps_coeff = lift_cepstr(1:50);
+	mag_spec = fft(ceps_coeff, 41500);
+	mag_spec = mag_spec(1:20250);
+	plot(real(mag_spec));
+	input("weiter")
 
-% Formantenbestimmung
-k = 1;
-formant = zeros(100,1);
-while length(formant)<101
-	for i = 2:(length(mag_spec)-1)
-		if mag_spec(i-1)<mag_spec(i) && mag_spec(i)>mag_spec(i+1)
-			formant(k) = mag_spec(k);
-			k = k+1;
-		else
-			continue;
-		end
-
-	end
+	% Maximabestimmung
+	%[pks, loc] = findpeaks(real(mag_spec), "DoubleSided");
 end
-disp(length(formant))
-
 % TODO: Maximumsbestimmung, Automatische Bearbeitung aller WAV-Files im Ordner
-
-
-% offene Fragen: wie naechste Fensterung vornehmen?
-% sind die Maxima der anschliessenden DFT die Formanten?
 % Wie genau wird die Bandbreite der Formanten abgelesen?
