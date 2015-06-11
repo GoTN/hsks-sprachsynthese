a25eb62ef5c5865e36359902eef839d8cdf48620
funjetfan <petersteiner92@t-online.de>
2015-06-12 01:17:46 +0200

    Realteil hat Amplitudendichtespektrum, mehr morgen

--- cepstrumanalysis.m
+++ cepstrumanalysis.m
@@ -15,7 +15,7 @@ lautliste={'a'};
 
 
 % Anzahl der Punkte für die spaetere FFT
-nf=2^14; %solange das auch immer ausreicht, sonst dynamisch
+nf=2^11; %solange das auch immer ausreicht, sonst dynamisch
 
 for datei = 1:numel(lautliste)   % loop over wave files
 	%signal = wavread (x(audio_files).name);   % read audio file
@@ -23,34 +23,38 @@ for datei = 1:numel(lautliste)   % loop over wave files
 	% Normierung, zeitliche Begrenzung der Aufnahme
 	%signal = signal./max(abs(signal));
 	signal = signal((end-nf)/2:(end+nf-1)/2);
+  figure;
+  plot(signal);
 	df=Fs/nf;
 	f=[0:df:(nf-1)*df];
-	% Fensterung der Aufnahme mit einem Hanning-Fenster
-	h_window = hanning(nf);
+	% Fensterung der Aufnahme mit einem Hamming-Fenster
+	h_window = hamming(nf);
 	signal = signal.*h_window;
-
 	% Berechnung des cepstrums
-	spectrum = fft(signal);
-	log_spectrum = log10(abs(spectrum));
+	log_spectrum = log10(abs(fft(signal)));
+  figure;
+  plot(log_spectrum);
 	cepstrum = ifft(log_spectrum);
 	%x_axis = (1:size(cepstrum));
 	
 	% Fensterung des cepstrums, da das Cepstrum symmetrisch ist
-	%cepstrum = cepstrum(1:size(cepstrum)/2);
-	figure;
-	plot(abs(cepstrum));
+	cepstrum = cepstrum(1:size(cepstrum)/2);
+  figure;
+	plot(real(cepstrum));
 	%plot(cepstrum);
 	% Liftering (Filterung des Vokaltraktsignals mit Formanten)
 	%lift = zeros(length(cepstrum),1);
 	%lift(1:50) = 1;
 	%lift_cepstr = real(cepstrum.*lift);	
 	%ceps_coeff = lift_cepstr(1:50);		%unnoetig, geht alles in einer zeile
-	ceps_coeff=abs(cepstrum(1:128));
-	mag_spec = fft(ceps_coeff);
-	df=Fs/length(mag_spec);
-	f=[0:df:(length(mag_spec)-1)*df];
+	ceps_coeff=real(cepstrum(1:128));
+	mag_spec = real(fft(ceps_coeff));
+  l=length(mag_spec);
+  mag_spec = mag_spec(1:64);
+	df=Fs/l;
+	f=[0:df:(l-1)/2*df];
 	figure;
-	plot(f,abs(mag_spec));
+	plot(f,mag_spec);
 	title(strcat('Cepstrum',' ',char(lautliste(datei))));
 	%input('weiter')
 
