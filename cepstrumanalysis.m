cba2c091200dcf8762be0de5c0efddb539d4717d
funjetfan <petersteiner92@t-online.de>
2015-06-17 22:15:48 +0200

    Cepstrumanalyse der Vokale abgeschlossen

--- cepstrumanalysis.m
+++ cepstrumanalysis.m
@@ -11,7 +11,7 @@
 
 %signal=wavread('a-marcus.wav');
 %x = dir("*.wav")
-lautliste={'a'};
+lautliste={'ue'};
 
 
 % Anzahl der Punkte für die spaetere FFT
@@ -22,33 +22,39 @@ for datei = 1:numel(lautliste)   % loop over wave files
 	[signal,Fs]=wavread(strcat(char(lautliste(datei)),'-marcus.wav'));
 	% Normierung, zeitliche Begrenzung der Aufnahme
 	signal = signal((end-nf)/2:(end+nf-1)/2);
-  figure;
-  plot(signal);
+  %figure;
+  %plot(signal);
 	df=Fs/nf;
 	f=[0:df:(nf-1)*df];
 	% Fensterung der Aufnahme mit einem Hamming-Fenster
 	h_window = hamming(nf);
 	signal = signal.*h_window;
 	% Berechnung des cepstrums
-	log_spectrum = log10(abs(fft(signal)));
-  figure;
-  plot(log_spectrum);
+	log_spectrum = 20*log10(abs(fft(signal)));
+  %figure;
+  %plot(log_spectrum);
 	cepstrum = ifft(log_spectrum);
 	
 	% Fensterung des cepstrums, da das Cepstrum symmetrisch ist
 	cepstrum = cepstrum(1:size(cepstrum)/2);
-  figure;
-	plot(real(cepstrum));
-	ceps_coeff=real(cepstrum(1:128));
+  %figure;
+	%plot(real(cepstrum));
+	ceps_coeff=real(cepstrum(1:256));
 	mag_spec = real(fft(ceps_coeff));
   l=length(mag_spec);
-  mag_spec = mag_spec(1:64);
+  mag_spec = mag_spec(1:128);
 	df=Fs/l;
 	f=[0:df:(l-1)/2*df];
 	figure;
 	plot(f,mag_spec);
+  title('Formanten f\"ur den Vokal a');
+  line([0 25000], [-12.4 -12.4])
+  line([0 25000], [-14.1 -14.1])
+  line([0 25000], [-13.774 -13.774])
+  xlabel('Frequenz')
+  ylabel('Amplitudendichtespektrum')
   [peak, loc] = findpeaks(mag_spec, "DoubleSided")
-	title(strcat('Cepstrum',' ',char(lautliste(datei))));
+
 	%input('weiter')
 
 	% Maximabestimmung
