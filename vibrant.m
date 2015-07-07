7bb97043dd34c388c0e417bfde34110c224a9a97
funjetfan <petersteiner92@t-online.de>
2015-07-07 10:25:38 +0200

    Funktion verändert

--- vibrant.m
+++ vibrant.m
@@ -1,43 +1,34 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%		HAUPTSEMINAR SPRACHSYNTHESE				%
-% 	Erzeugung eines Vibranten mittels einfacher Formantfilterung 	%
+%						HAUPTSEMINAR SPRACHSYNTHESE						%
+% Erzeugung eines Vibranten mittels einfacher Formantfilterung %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [y , f , B]=vibrant(laute,DUR,fs,syn)
+function [y , f , B]=vibrant(buchstaben,DUR,fs,syn)
 
 %%%%%			PARAMETER			%%%%%
-if (nargin==0) laute={'r'}			;end%Buchstaben
+if (nargin==0) buchstaben={'r'}			;end%Buchstaben
 if (nargin<=1) DUR=2; end %duration in sec
 if (nargin<=2) fs=44100; end %sampling freq in Hz
 %if (nargin<=3) B=[100 160]; end %bandwidth
-if (nargin<=3) syn=1;	end %bool, ob synthetisiert werden soll
+if (nargin<=3) syn=1;	end %%soll synthetisieren?
 
-Definition von Periodendauer, Grundfrequenz und Filterparameter
-Ts=1/fs;	%Dauer eines Samples
-f1=f2=f3=0;%Initialisierung f1-f3
-B1=B2=B3=0;%Initialisierung B1-B3
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-samples=ceil(DUR*fs); %Anzahl der Samples
-t=0:Ts:(samples-1)*Ts; %Zeitachse
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+Ts=1/fs;
+T_w = 0.04;
+f_w = 1/T_w;
+f0=150;	% Grundschwingung, Tonhoehe
+f1=f2=f3=0;%damit die in FKT definiert sind...
+B1=B2=B3=0;%damit die in FKT definiert sind...
+samples=ceil(DUR*fs);
+t=0:Ts:(samples-1)*Ts;
 x=sourcesignal('vibrant',DUR,fs);
+disp(buchstaben);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-f_w = 7; %Frequenz für Fensterfunktion
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for i=1:numel(laute)
-	laut=laute(i);
+for i=1:numel(buchstaben)
+	buchstabe=buchstaben(i);
 	
-	%Switch-Case-Struktur, zur einfachen Erweiterung des Programms
 	switch char(buchstabe)
 		case 'r'
-		% Aus Klatt-Paper entnommene Werte
 			f1=310;
 			f2=1060;
 			f3=1380;	
@@ -46,23 +37,19 @@ for i=1:numel(laute)
 			B3=120;
 	end
 	if(syn == 1)
-	
-	%Formantfilterung
 		y=formantfilter(x,Ts,f1,B1);	%1. Formantfilter
 		y=formantfilter(y,Ts,f2,B2);	%2. Formantfilter
 		y=formantfilter(y,Ts,f3,B3);	%3. Formantfilter
-		
-	%Definition eines Fensters nach Lehrbriefen
     N = length(y);
 		N1 = floor(.2*N);
 		wind_1 = zeros(1,N1);
-		wind_2 = 3*sin(2*pi*f_w*t(N1+1:N))+t(N1+1:N);
+		wind_2 = sin(2*pi*f_w*t(N1+1:N)).^2+(t(N1+1:N)-0.2*T_w)*f_w;
 		wind = [wind_1 wind_2];
-	%Fensterung des Signals
     y=y.*wind;
+    
+	else 
+		y=x;
 	end
-	%Normierung des Signals
 	y=y/max(y);
-	%Schreiben des Einzellauts in wav-Datei
 	wavwrite(y'/max(y),fs,strcat('vibrant-',char(buchstabe),'.wav'));
 end
