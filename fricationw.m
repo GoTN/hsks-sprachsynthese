a0ad7ca9c9bab862b126af21629ab8b302e1d343
Lzjtud <lizhongjuxd@gmail.com>
2015-07-14 20:14:52 +0200

    fricationw

--- fricationw.m
+++ fricationw.m
@@ -12,6 +12,9 @@ if (nargin<=1) DUR=2; end %duration in sec
 if (nargin<=2) fs=44100; end %sampling freq in Hz
 
 Ts=1/fs;
+t=0:Ts:DUR;
+t=t(1:(length(t)-1));
+f0=125;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -23,6 +26,7 @@ x02=sourcesignal('vokal',DUR,fs);		%Erzeugung des zweiten Anregungssignal
 
 [b a] = butter(2,100/(.5*fs),'low');	%Manipulation des ersten Anregungssignals im Frequenzbereich mit Tiefpassfilter
 y1 = filter(b,a,x01);
+y1 = y1.*(square(2*pi*f0*t,50)+1)/2;
 
 w1 = formantfilter(x02,Ts,251,100);		%1. Formantfilter
 w1 = formantfilter(w1,Ts,1164,300);		%2. Formantfilter
